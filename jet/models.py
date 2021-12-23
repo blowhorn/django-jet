1089fdbbd87ecb393dcf863312bb5eb3b629b242
santosh-desai-bh <53857354+santosh-desai-bh@users.noreply.github.com>
2021-12-23 20:45:19 +0530

    Update models.py
    ImportError: cannot import name 'python_2_unicode_compatible' from 'django.utils.encoding' (/usr/local/lib/python3.9/site-packages/django/utils/encoding.py
    django 3.2 related

--- jet/models.py
+++ jet/models.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.utils import timezone
-from django.utils.encoding import python_2_unicode_compatible
 from django.utils.translation import ugettext_lazy as _
 
 
