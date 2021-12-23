f7e874cefe000a60bb55c469bd7569f2091f13f1
santosh-desai-bh <53857354+santosh-desai-bh@users.noreply.github.com>
2021-12-23 20:46:16 +0530

    Update models.py

--- jet/models.py
+++ jet/models.py
@@ -2,8 +2,6 @@ from django.db import models
 from django.utils import timezone
 from django.utils.translation import ugettext_lazy as _
 
-
-@python_2_unicode_compatible
 class Bookmark(models.Model):
     url = models.URLField(verbose_name=_('URL'))
     title = models.CharField(verbose_name=_('title'), max_length=255)
@@ -18,8 +16,6 @@ class Bookmark(models.Model):
     def __str__(self):
         return self.title
 
-
-@python_2_unicode_compatible
 class PinnedApplication(models.Model):
     app_label = models.CharField(verbose_name=_('application name'), max_length=255)
     user = models.PositiveIntegerField(verbose_name=_('user'))
